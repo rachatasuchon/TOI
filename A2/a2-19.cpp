677992936449d494ffe78ecaa46ce3e6cd5f5abe
rachatasuchon <rachatasuchon@gmail.com>
2026-05-31 18:37:24 +0700

    Update a2-19.cpp

--- A2/a2-19.cpp
+++ A2/a2-19.cpp
@@ -1,39 +1,39 @@
-#include <bits/stdc++.h>
-using namespace std;
+    #include <bits/stdc++.h>
+    using namespace std;
 
-int main() {
-    cin.tie(0)->sync_with_stdio(0);
-    string text;
-    bool foundB = false;
-    int current=0, max=0,Pos=-1;
-    cin >> text;
-    for(int i=0;i<text.size();i++) {
-        if(text[i]=='B'||text[i]=='b') {
-            if(!foundB) {
-                foundB = true;
-                Pos = i;
-            }
-            int current =0;
-            int j = i+1;
-            while(j<text.size() && text[j]!='B' && text[j]!='b') {
+    int main() {
+        cin.tie(0)->sync_with_stdio(0);
+        string text;
+        bool foundB = false;
+        int max=0,current=0,Pos=-1;
+        cin >> text;
+        for(int i=0;i<text.size();i++) {
+            if(text[i]=='B'||text[i]=='b') {
+                if(!foundB) {
+                    foundB=true;
+                    Pos=i;
+                }
+            current=0;
+            int j=i+1;
+            while(j<text.size()&&(text[j]=='U'||text[j]=='u')) {
                 current++;
                 j++;
-            }  
-        }   
-    }
-    if(max>=2) {
-        cout<<"Yes "<<max;
-    } else if(foundB) {
-        for(int i=Pos+1;i<text.size();i++) {
-            text[i] = 'U';
+            }        
+            if(current>max) {max=current;}
+            }
+        }  
+        if(max>=2) {
+            cout<<"Yes "<<max;
+        }else if(foundB) {
+            for (int j=Pos+1;j<text.size();j++) {
+                text[j]='U';
+            }
+            cout<<text;
+        } else {
+            string BUU = "BUU";
+            for(int i=0;i<text.size();i++) {
+                cout<<BUU[i%3];
+            }
         }
-        cout<<text;
-    } else {
-        string BUU = "BUU";
-        for(int i=0;i<text.size();i++) {
-            text[i] = BUU[i%3];
-        }        
-        cout<<BUU;
+        return 0;
     }
-    return 0;
-}
